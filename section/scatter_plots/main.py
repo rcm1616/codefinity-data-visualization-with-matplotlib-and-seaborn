1120d97ee5104a4c02b7b64f587faa1586f35163
rcm1616 <>
2026-04-08 07:37:46 +0000

    Scatter plots

--- section/scatter_plots/main.py
+++ section/scatter_plots/main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 x = np.array([6, 1, 8, 20, 13, 4, 16, 5, 11, 10])
 # Square the elements of the x array
-y = ___ ** 2
+y = x ** 2
 # Create a scatter plot
-___.___(___, ___, s=___)
+plt.scatter(x, y, s=70)
 plt.show()
